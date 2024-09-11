3f5d0589788da8161964dabdf5b04d701bdb62dc
KIMSEI1124 <workju1124@gmail.com>
2024-09-12 04:20:00 +0900

    Refactor(#22): type to interface

--- src/@types/Member.d.ts
+++ src/@types/Member.d.ts
@@ -5,10 +5,10 @@ interface SearchMemberForCreateGroupData {
   profile_image_url: string | null;
 }
 
-type MemberProfileData = {
+interface MemberProfileData {
   id: number;
   email: string;
   nickname: string;
   profile_image_url: string | null;
   main_color: string;
-};
+}
