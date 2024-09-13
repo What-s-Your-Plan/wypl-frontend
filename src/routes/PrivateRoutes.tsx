c93603b52878fa7f2ef35d2043647e2772c680c0
KIMSEI1124 <workju1124@gmail.com>
2024-09-13 20:39:39 +0900

    Refactor(#22): 라우터 수정

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -1,19 +1,25 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 
 import { BROWSER_PATH } from '@/constants/Path';
 import useJsonWebTokensStore from '@/stores/TokenStore';
 
-
 function PrivateWrapper() {
   const navigate = useNavigate();
   const { accessToken, refreshToken } = useJsonWebTokensStore();
+  const isMounted = useRef(false);
 
   useEffect(() => {
-    if (accessToken === null || refreshToken === null) {
+    if (isMounted.current) {
+      return;
+    }
+
+    if (!accessToken || !refreshToken) {
       navigate(BROWSER_PATH.LANDING);
     }
-  }, []);
+
+    isMounted.current = true; // 플래그 설정
+  }, [accessToken, refreshToken, navigate]); // 의존성 배열을 안전하게 추가
 
   return <Outlet />;
 }
