552c65ca025d26a6419f230898dd30064d5deddc
KIMSEI1124 <workju1124@gmail.com>
2024-09-15 18:47:47 +0900

    Feat(#22): Animation Components StoryBook 작성

---
+++ src/stories/animation/Loading.stories.ts
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Loading from '@/components/animation/Loading.tsx';
+
+const meta: Meta<typeof Loading> = {
+  title: 'Animation/Loading',
+  component: Loading,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
